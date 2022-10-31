23c85ab94afec502b28b14c543be7dd13b7197cb
Yeakob <yeakob677@gmail.com>
2022-10-31 14:54:55 +0600

    Professionals

--- frontendls/src/Proff/Prof.jsx
+++ frontendls/src/Proff/Prof.jsx
@@ -10,7 +10,7 @@ const Prof = () => {
     <div>
      
      <div className='w-full h-auto bg-slate-100 py-4 text-start px-10 text-md font-mono text-slate-600 font-semibold'>
-        <a href="">Home</a> / Professionals
+        <a className='hover:text-black' href="">Home</a> / Professionals
       </div>
       <div className='w-full h-auto flex flex-col justify-center items-center flex-wrap'>
         <div className='w-full h-auto px-10 pt-8'>
@@ -27,59 +27,61 @@ const Prof = () => {
           <button className='duration-500 text-lg font-mono py-2 font-semibold hover:text-black text-slate-500 px-3 tracking-tight' onClick={()=>{setactive('Sexual')}}>Sexual Health</button>
            </div> 
            <div className='w-full h-auto p-8 flex flex-row flex-wrap justify-center items-center lg:justify-start lg:items-start lg:pl-14'>
-          {active==='ALL' && Info.map((items)=>{ return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> })} 
+          {active==='ALL' && Info.map((items)=>{ return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> })} 
           {active==='Child' && Info.map((items) =>
           {
           if(items.prof==='CHILD-ADOLESCENT-PSYCHIATRIST')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null
           }
-         )}
+          
+          )}
           {active==='Counselor' && Info.map((items)=>{
           if(items.prof==='COUNSELOR')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null})}
           {active==='Dermatologist' && Info.map((items)=>{
           if(items.prof==='DERMATOLOGIST')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null})}
           {active==='Therapist' && Info.map((items)=>{
           if(items.prof==='DEVELOPMENTAL-THERAPIST')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null})}
           {active==='ENT' && Info.map((items)=>{
           if(items.prof==='ENT')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null })}
           {active==='Gynecologist' && Info.map((items)=>{
           if(items.prof==='GYNECOLOGIST')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null})}
           {active==='Internal' && Info.map((items)=>{
           if(items.prof==='INTERNAL-MEDICINE')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null})}
           {active==='Psychiatrist' && Info.map((items)=>{
           if(items.prof==='PSYCHIATRIST')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null})}
           {active==='Psychologist' && Info.map((items)=>{
           if(items.prof==='PSYCHOLOGIST')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null})}
           {active==='Sexual' && Info.map((items)=>{
           if(items.prof==='Sexual Health')
           {
-           return <Card img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
-          })}
+           return <Card key={items.id} img={items.img} prof={items.prof} name={items.name} degree={items.degree} /> }
+           return null})}
          </div> 
       </div>
     </div>
