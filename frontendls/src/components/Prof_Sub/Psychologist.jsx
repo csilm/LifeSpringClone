518db7ab291f7c1043347a170f471b0615ecb67a
Yeakob <yeakob677@gmail.com>
2022-11-03 14:22:02 +0600

    Professionals Sub

--- frontendls/src/components/Prof_Sub/Psychologist.jsx
+++ frontendls/src/components/Prof_Sub/Psychologist.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Info from '../Proff/Info.json'
 import Card from '../Proff/Card'
+import './style.css'
 
 const Psychologist = () => {
     return (
         <div>
-            <div className='w-full h-auto bg-slate-100 py-4 text-start px-10 text-md font-mono text-slate-600 font-semibold'>
-                <a className='hover:text-black' href='#h'>Home</a> / Professionals / Psychologist
-            </div>
+            <div className='w-full flex flex-row h-auto bg-slate-100 py-4 text-start px-10 text-md font-mono text-slate-600 font-semibold'>
+              <a className='hover:text-black mark flex flex-col flex-wrap first-letter mr-2' href='#h'>Home  <div className='line'></div></a> / <div className='hover:text-black mark flex flex-col flex-wrap first-letter mr-2 cursor-pointer px-2'>Professionals <div className='line1'></div></div> / Psychologists
+          </div>
             <div className='w-full h-auto p-8 flex flex-row flex-wrap justify-center items-center lg:justify-start lg:items-start lg:pl-14'>
             {Info.map((items) => {
             if (items.prof === 'PSYCHOLOGIST') {
