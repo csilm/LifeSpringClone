1ed76dcbe6ad3d9c4f4def52fc56277b66f16072
Showrav-das <showravdas8@gmail.com>
2022-11-04 15:15:07 +0600

    commit

--- frontendls/src/App.js
+++ frontendls/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
+      {/*<h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+    </h1>*/}
+    <Homepage/>
     </div>
   );
 }
--- frontendls/src/components/Homepage/Homepage.jsx
+++ frontendls/src/components/Homepage/Homepage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Banner from './Banner';
+import Courses from './Courses';
+import Video from './Video';
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+    {/*<div>
+      Homepage
+      </div>*/}
+      
+      <Banner/>
+      <Courses />
+      <Video/>
+    </>
   )
 }
 
