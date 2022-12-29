c36c090237b1eba218842b3d0a4ed2583e8469c4
unknown <isfakahmed163@gmial.com>
2022-12-29 11:32:54 +0600

    mongoose.set is true

--- Backendls/server.js
+++ Backendls/server.js
@@ -40,7 +40,9 @@ app.use("/api/blogs", blogsRoutes);
 
 
 // connect to DB
-mongoose
+mongoose.set('strictQuery', true);
+try {
+  mongoose
   .connect(`${process.env.MONGO_DB_URL}`)
   .then(() => {
     // listen for request
@@ -51,57 +53,61 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+} catch (error) {
+  console.log(err);
+}
 
-  app.use("/ssl",async(req,res,next)=>{
-    const data = {
-        total_amount: 300,
-        currency: 'EUR',
-        tran_id: 'REF123',
-        success_url:`${process.env.URL}/ssl-payment-success`,
-        fail_url: `${process.env.URL}/ssl-payment-failure`,
-        cancel_url: `${process.env.URL}/ssl-payment-cancel`,
-        ipn_url: `${process.env.URL}/ssl-payment-ipn`,
-        shipping_method: 'Courier',
-        product_name: 'Computer.',
-        product_category: 'Electronic',
-        product_profile: 'general',
-        cus_name: 'Customer Name',
-        cus_email: 'cust@yahoo.com',
-        cus_add1: 'Dhaka',
-        cus_add2: 'Dhaka',
-        cus_city: 'Dhaka',
-        cus_state: 'Dhaka',
-        cus_postcode: '1000',
-        cus_country: 'Bangladesh',
-        cus_phone: '01711111111',
-        cus_fax: '01711111111',
-        ship_name: 'Customer Name',
-        ship_add1: 'Dhaka',
-        ship_add2: 'Dhaka',
-        ship_city: 'Dhaka',
-        ship_state: 'Dhaka',
-        ship_postcode: 1000,
-        ship_country: 'Bangladesh',
-        multi_card_name: 'mastercard',
-        value_a: 'ref001_A',
-        value_b: 'ref002_B',
-        value_c: 'ref003_C',
-        value_d: 'ref004_D'
-    };
-    const sslcommer = new SSLCommerzPayment(process.env.STORE_ID ,process.env.PASS, false ) //true for live default false for sandbox
-    sslcommer.init(data).then(data => {
-        //process the response that got from sslcommerz 
-        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
-      if(data ?.GatewayPageURL){
-        return res.status(200).redirect(data ?.GatewayPageURL)
-      }
-      else
-      {
-        return res.status(400).json({
-            message: "SSL not success"
-        })
-      }
-    });
+
+app.use("/ssl",async(req,res,next)=>{
+  const data = {
+      total_amount: 300,
+      currency: 'EUR',
+      tran_id: 'REF123',
+      success_url:`${process.env.URL}/ssl-payment-success`,
+      fail_url: `${process.env.URL}/ssl-payment-failure`,
+      cancel_url: `${process.env.URL}/ssl-payment-cancel`,
+      ipn_url: `${process.env.URL}/ssl-payment-ipn`,
+      shipping_method: 'Courier',
+      product_name: 'Computer.',
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name: 'Customer Name',
+      cus_email: 'cust@yahoo.com',
+      cus_add1: 'Dhaka',
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone: '01711111111',
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
+      multi_card_name: 'mastercard',
+      value_a: 'ref001_A',
+      value_b: 'ref002_B',
+      value_c: 'ref003_C',
+      value_d: 'ref004_D'
+  };
+  const sslcommer = new SSLCommerzPayment(process.env.STORE_ID ,process.env.PASS, false ) //true for live default false for sandbox
+  sslcommer.init(data).then(data => {
+      //process the response that got from sslcommerz 
+      //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+    if(data ?.GatewayPageURL){
+      return res.status(200).redirect(data ?.GatewayPageURL)
+    }
+    else
+    {
+      return res.status(400).json({
+          message: "SSL not success"
+      })
+    }
+  });
 })
 
 app.post("/ssl-payment-success",async(req,res,next)=>
