525a0d182e41b4cd7631bd60c8ea44bb0dee5d2e
shafiuljony <shafiuljony12@gmail.com>
2022-12-31 20:26:06 +0600

    sidebar text hidden

--- frontendls/src/components/Dashboard/Sidebar.jsx
+++ frontendls/src/components/Dashboard/Sidebar.jsx
@@ -33,13 +33,13 @@ const Sidebar = () => {
           : "shadow space-y-4 pt-4 min-h-screen min-w-[280px]  bg-white z-40"
       }
     >
-      <div className="flex justify-end items-center">
+      <div className="flex  items-center">
         {open ? (
           <button
             onClick={() => setOpen(!open)}
-            className="px-4 block cursor-pointer"
+            className="px-4 ml-10 block cursor-pointer"
           >
-            <AiOutlineBars className="text-2xl text-black" />
+            <AiOutlineBars className="text-2xl text-black " />
           </button>
         ) : (
           <div className="flex justify-between">
@@ -75,8 +75,8 @@ const Sidebar = () => {
               : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold"
           }
         >
-          <MdOutlineDashboard className="text-base" />
-          <span className="ml-3">Dashboard</span>
+          <MdOutlineDashboard className="text-2xl" />
+          <span className={`ml-3 ${open && "scale-0"}`}>Dashboard</span>
         </NavLink>
         {user?.user && user?.role === "admin" && (
           <NavLink
@@ -87,7 +87,7 @@ const Sidebar = () => {
                 : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
             }
           >
-            <SiProtocolsdotio className="text-base" />
+            <SiProtocolsdotio className="text-2xl" />
             <span className="ml-3">Professionals</span>
           </NavLink>
         )}
@@ -99,8 +99,8 @@ const Sidebar = () => {
               : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
           }
         >
-          <AiOutlineNodeIndex className="text-base" />
-          <span className="ml-3">Category</span>
+          <AiOutlineNodeIndex className="text-2xl" />
+          <span className={`ml-3 ${open && "scale-0"}`}>Category</span>
         </NavLink>
         {user?.user && user?.role === "admin" && (
           <NavLink
@@ -111,7 +111,7 @@ const Sidebar = () => {
                 : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
             }
           >
-            <AiOutlineContainer className="text-base" />
+            <AiOutlineContainer className="text-2xl" />
             <span className="ml-3">Add Courses</span>
           </NavLink>
         )}
@@ -137,8 +137,8 @@ const Sidebar = () => {
               : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
           }
         >
-          <AiOutlineUserAdd className="text-base" />
-          <span className="ml-3">My Profile</span>
+          <AiOutlineUserAdd className="text-2xl" />
+          <span className={`ml-3 ${open && "scale-0"}`}>My Profile</span>
         </NavLink>
 
         <NavLink
@@ -149,8 +149,8 @@ const Sidebar = () => {
               : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
           }
         >
-          <BsBook className="text-base" />
-          <span className="ml-3">Enroll Course</span>
+          <BsBook className="text-2xl" />
+          <span className={`ml-3 ${open && "scale-0"}`}>Enroll Course</span>
         </NavLink>
 
         <NavLink
@@ -161,8 +161,8 @@ const Sidebar = () => {
               : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
           }
         >
-          <AiOutlineHeart className="text-base" />
-          <span className="ml-3">Wishlist</span>
+          <AiOutlineHeart className="text-2xl" />
+          <span className={`ml-3 ${open && "scale-0"}`}>Wishlist</span>
         </NavLink>
 
         <NavLink
@@ -173,8 +173,8 @@ const Sidebar = () => {
               : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
           }
         >
-          <AiOutlineStar className="text-base" />
-          <span className="ml-3">Reviews</span>
+          <AiOutlineStar className="text-2xl" />
+          <span className={`ml-3 ${open && "scale-0"}`}>Reviews</span>
         </NavLink>
 
         <NavLink
@@ -185,8 +185,8 @@ const Sidebar = () => {
               : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
           }
         >
-          <AiOutlineShoppingCart className="text-base" />
-          <span className="ml-3">Purchase History</span>
+          <AiOutlineShoppingCart className="text-2xl" />
+          <span className={`ml-3 ${open && "scale-0"}`}>Purchase History</span>
         </NavLink>
 
         <hr />
@@ -199,16 +199,16 @@ const Sidebar = () => {
               : "text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] font-semibold mt-2"
           }
         >
-          <AiOutlineSetting className="text-base" />
-          <span className="ml-3">Settings</span>
+          <AiOutlineSetting className="text-2xl" />
+          <span className={`ml-3 ${open && "scale-0"}`}>Settings</span>
         </NavLink>
 
         <button
           onClick={() => logout()}
           className="text-[#99CC33] flex justify-start items-center space-x-2 rounded-md p-3 pl-8 hover:bg-[#EDF0F9] w-full"
         >
-          <AiOutlineLogout className="text-base text-[#99CC33]" />
-          <span className="ml-3">Logout</span>
+          <AiOutlineLogout className="text-2xl text-[#99CC33]" />
+          <span className={`ml-3 ${open && "scale-0"}`}>Logout</span>
         </button>
       </div>
     </div>
